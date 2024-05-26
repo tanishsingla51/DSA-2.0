e74d9e0f4ef6f4d32864cb6df8428728f7491095
tanishsingla51 <131950694+tanishsingla51@users.noreply.github.com>
2024-05-26 14:18:45 +0530

    different approach

---
+++ Recursion_Type_2/index_4.java
@@ -0,0 +1,26 @@
+package Recursion_Type_2;
+
+import java.util.*;
+
+public class index_4 {
+
+    public static void combination(int n, String ans) {
+
+        if (n == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        combination(n - 1, ans + "0");
+        combination(n - 1, ans + "1");
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int n = 3;
+
+        combination(n, "");
+    }
+}
